146081d2639069d5ea84b5da761916e98482e71f
dopafish <ywd416795838@gmail.com>
2023-06-01 21:46:33 +0200

    'Distributed'

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -8,20 +8,44 @@
 
 import weakref
 
+import threading
+#import websocket
+import requests
+
+import xmlrpc.client
+
+import sys
+sys.path.insert(0, '../')
+from joint_control.keyframes import *
+import logging
+
+
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
     '''
     def __init__(self, obj):
         self.proxy = weakref.proxy(obj)
+        #self.server.register_function(self.get_angle, "get_angle")
 
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
 
+
+        threading.Thread(target=self.proxy.execute_keyframes, args=(keyframes,)).start()
+
+
+
+        #threading.Thread(target=self.proxy)
+
+
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
 
+        threading.Thread(target=self.proxy.set_transform, args=(effector_name, transform)).start()
+
+        #threading.Thread
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
@@ -29,38 +53,127 @@ class ClientAgent(object):
     # YOUR CODE HERE
     def __init__(self):
         self.post = PostHandler(self)
-    
+
+        #self.ws = websocket.WebSocket()
+        #self.ws.connect("ws://localhost:8000")
+
+        #self.server_url = "http://localhost:8000"
+
+        self.server = xmlrpc.client.ServerProxy("http://localhost:8001")
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
-    
+
+        #response = requests.get(self.server_url + "/get_angle", params={"joint_name": joint_name})
+        #response.raise_for_status()
+        #return response.json()
+        '''''
+        try:
+            #response = requests.get(self.server_url + "/get_angle", params={"joint_name": joint_name})
+            response = requests.get(self.server_url + "/get_angle", params={"joint_name": joint_name})
+            response.raise_for_status()
+            return response.json()  # Assuming the server response is JSON
+        except Exception as e:
+            return("Error in get_angle:", e)
+        '''''
+
+
+
+        try:
+            return self.server.rpc_get_angle(joint_name)
+        except Exception as e:
+            logging.error("Error in get_angle: %s", e)
+
+
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
 
+        try:
+            return self.server.rpc_set_angle(joint_name, angle)
+        except Exception as e:
+            logging.error("Error in set_angle: %s", e)
+
+
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        '''''
+        try:
+            response = requests.get(self.server_url + "/get_posture")
+            response.raise_for_status()
+            return response.json()  # Assuming the server response is JSON
+        except Exception as e:
+            return("Error in get_posture:", e)
+        '''''
+
+
+        try:
+            return self.server.rpc_get_posture()
+        except Exception as e:
+             logging.error("Error in get_posture: %s", e)
+
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        try:
+            return self.server.rpc_execute_keyframes(keyframes)
+        except Exception as e:
+            logging.error("Error in execute_keyframes: %s", e)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        '''''
+        try:
+            response = requests.get(self.server_url + "/get_transform", params={"name": name})
+            response.raise_for_status()
+            return response.json()  # Assuming the server response is JSON
+        except Exception as e:
+            print("Error in get_transform:", e)
+        '''''
+
+        try:
+            return self.server.rpc_get_transform(name)
+        except Exception as e:
+            #print("Error in get_transform")
+            logging.error("Error in get_transform: %s", e)
+
+
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
 
+        '''''
+        try:
+            data = {'effector_name': effector_name, 'transform': transform}
+            response = requests.post(self.server_url + "/set_transform", json=data)
+            response.raise_for_status()
+        except Exception as e:
+            print("Error in set_transform:", e)
+        '''''
+        try:
+            return self.server.rpc_set_transform(effector_name, transform)
+        except Exception as e:
+            logging.error("Error in set_transform: %s",e)
+
 if __name__ == '__main__':
     agent = ClientAgent()
     # TEST CODE HERE
-
-
+    print(agent.get_angle("HeadYaw"))
+    print(agent.get_posture())
+    print(agent.get_transform("HeadYaw"))
+    keyframes = hello()
+    print("Executing keyframe hello")
+    a = agent.execute_keyframes(keyframes)
+    print(a)
+    a = agent.execute_keyframes(keyframes)
+    print(a)
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -16,42 +16,110 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler as RequestHandler
+
 from inverse_kinematics import InverseKinematicsAgent
+import json
+import threading
+import numpy as np
+from agent_client import PostHandler
+
+
+import sys
+sys.path.insert(0, '../')
+from joint_control.keyframes import *
+
 
 
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
-    
+
+
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+
+        self.post = PostHandler(self)
+        self.server = SimpleXMLRPCServer(("localhost", 8001))
+
+        self.server.register_function(self.get_angle, "get_angle")
+        self.server.register_function(self.set_angle, "set_angle")
+        self.server.register_function(self.get_posture, "get_posture")
+        self.server.register_function(self.execute_keyframes, "execute_keyframes")
+        self.server.register_function(self.get_transform, "get_transform")
+        self.server.register_function(self.set_transform, "set_transform")
+
+        try:
+            t = threading.Thread(target=self.server.serve_forever)
+            t.start()
+        except Exception as e:
+            print("Error starting server: ", e)
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        if joint_name in self.joint_names and joint_name in self.perception.joint:
+            return self.perception.joint[joint_name]
+        else:
+            return "Invalid joint name"
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        if joint_name in self.joint_names and joint_name in self.target_joints:
+            self.target_joints[joint_name] = angle
+        else:
+            return "Invalid joint name"
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.posture
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = keyframes
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
 
+        with threading.Lock():
+            transform = self.transforms[name].tolist()  # convert the numpy array to a python list
+        json_transform = json.dumps(transform)
+        return json_transform
+
+
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+
+        if isinstance(transform, str):
+            transform = np.array(json.loads(transform))
+        elif isinstance(transform, list):
+            transform = np.array(transform)
+        else:
+            return "Invalid transform format"
+
+        if effector_name not in self.joint_names:
+            return "Invalid effector name"
+
+        try:
+            joint_angles = self.inverse_kinematics(effector_name, transform)
+            if joint_angles is None or len(joint_angles) != len(self.joint_names):
+                return "Error in inverse kinematics"
+            else:
+                self.target_joints = dict(zip(self.joint_names, joint_angles))
+                return True
+        except Exception as e:
+            return f"Error in setting transform: {e}"
 
 if __name__ == '__main__':
     agent = ServerAgent()
